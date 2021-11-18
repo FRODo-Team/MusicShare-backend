1908ce482a7f663ff0648bc62bde8d1007245bd1
rflban <nabiev.alsn@yandex.ru>
2021-11-18 17:22:53 +0300

    MUS-23: add endpoints base class and user controller
    (cherry picked from commit ad65477dd643b201ff60163ea2b5e228eebaed9e)

---
+++ lib/mus-internal/mus-controller/include/mus-controller/request.h
@@ -0,0 +1,19 @@
+#ifndef MUS_MUSINTERNAL_MUSCONTROLLER_REQUEST_H_
+#define MUS_MUSINTERNAL_MUSCONTROLLER_REQUEST_H_
+
+#include <map>  // std::map
+#include <string>  // std::string
+#include <optional>  // std::optional
+
+namespace music_share {
+
+template <typename Body>
+struct Request {
+    std::map<std::string, std::string> parameters;
+    std::map<std::string, std::string> headers;
+    std::optional<Body> body;
+};
+
+}  // namespace music_share
+
+#endif  // MUS_MUSINTERNAL_MUSCONTROLLER_REQUEST_H_
