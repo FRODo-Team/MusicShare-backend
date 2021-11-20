02b2ec34096e4efe3950612e473b93279cbdde50
Rostislav V <sunz89@yandex.ru>
2021-11-20 16:39:58 +0300

    MUS-26: update tests for postgres repositories
    (cherry picked from commit 15e4e3c5ad90f928289aa005c2c0646799b65a47)

--- lib/mus-internal/mus-repo-postgres/tests/unittests/test_song_repository_postgres.h
+++ lib/mus-internal/mus-repo-postgres/tests/unittests/test_song_repository_postgres.h
@@ -44,6 +44,18 @@ TEST_F(TestSongRepositoryPostgres, Find) {
     }
 }
 
+TEST_F(TestSongRepositoryPostgres, FindNotExistingSong) {
+    // arrange
+    const uint32_t song_id_to_find = 1123123123;
+
+    // act
+    auto repo = std::make_unique<SongRepositoryPostgres>(s_ConnectionString);
+    std::optional<Song> actual_song = repo->Find(song_id_to_find);
+
+    // assert
+    EXPECT_FALSE(actual_song.has_value());
+}
+
 TEST_F(TestSongRepositoryPostgres, Insert) {
     // arrange
     const uint32_t id = 2;
@@ -71,16 +83,43 @@ TEST_F(TestSongRepositoryPostgres, Insert) {
     EXPECT_EQ(expected_song.GetId(), song_to_insert.GetId());
 }
 
+TEST_F(TestSongRepositoryPostgres, Update) {
+    // arrange
+    const uint32_t id = 2;
+
+    Song song_to_update("UPDATED_TITLE", "lildude", "14.88",
+                        "/trash/UPDATED_NAME.mp4", 1917, "unknown", "RAP", id);
+
+    // act
+    auto repo = std::make_unique<SongRepositoryPostgres>(s_ConnectionString);
+    repo->Update(song_to_update);
+
+    // assert
+    std::optional<Song> song_updated = repo->Find(id);
+    EXPECT_TRUE(song_updated.has_value());
+    if (song_updated) {
+        EXPECT_STREQ(song_updated->GetTitle().c_str(), song_to_update.GetTitle().c_str());
+        EXPECT_STREQ(song_updated->GetArtist().c_str(), song_to_update.GetArtist().c_str());
+        EXPECT_STREQ(song_updated->GetDuration().c_str(), song_to_update.GetDuration().c_str());
+        EXPECT_STREQ(song_updated->GetUrl().c_str(), song_to_update.GetUrl().c_str());
+        EXPECT_EQ(song_updated->GetYear(), song_to_update.GetYear());
+        EXPECT_STREQ(song_updated->GetAlbum()->c_str(), song_to_update.GetAlbum()->c_str());
+        EXPECT_STREQ(song_updated->GetGenre()->c_str(), song_to_update.GetGenre()->c_str());
+        EXPECT_EQ(song_updated->GetId(), song_to_update.GetId());
+    }
+}
+
+
 TEST_F(TestSongRepositoryPostgres, Delete) {
     // arrange
     const uint32_t id = 2;
-    const Song song_to_update("UPDATED_NAME", "lildude", "14.88",
+    const Song song_to_delete("UPDATED_NAME", "lildude", "14.88",
                               "/trash/nonsense.mp4", 1917, "unknown", "hip-hop",
                               id);
 
     // act
     auto repo = std::make_unique<SongRepositoryPostgres>(s_ConnectionString);
-    repo->Delete(song_to_update);
+    repo->Delete(song_to_delete);
 
     // assert
     std::optional<Song> song_updated = repo->Find(id);
