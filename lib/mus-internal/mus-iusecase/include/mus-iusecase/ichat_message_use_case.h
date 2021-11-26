957386f82c10aab0eee74d12912415668ff02315
Darya Vlaskina <vlaskinadarja@rambler.ru>
2021-11-26 14:27:45 +0300

    MUS-27: add chat message use case

---
+++ lib/mus-internal/mus-iusecase/include/mus-iusecase/ichat_message_use_case.h
@@ -0,0 +1,30 @@
+#ifndef MUS_INTERNAL_MUS_IUSECASE_ICHAT_MESSAGE_USE_CASE_H_
+#define MUS_INTERNAL_MUS_IUSECASE_ICHAT_MESSAGE_USE_CASE_H_
+
+#include <vector>
+
+#include "mus-dto/message_response_dto.h"
+#include "mus-dto/message_request_dto.h"
+
+namespace music_share {
+
+    class IChatMessageUseCase {
+    public:
+        IChatMessageUseCase() = default;
+
+        IChatMessageUseCase(const IChatMessageUseCase &chat_message_use_case) = default;
+
+        IChatMessageUseCase &operator=(const IChatMessageUseCase &chat_message_use_case) = default;
+
+        virtual bool SendMessage(MessageRequestDTO message, uint32_t chat_id,
+                                 uint32_t user_id) = 0;
+
+        virtual std::vector <MessageResponseDTO> GetUserMessages(uint32_t user_id,
+                                                                 uint32_t chat_id) = 0;
+
+        virtual ~IChatMessageUseCase() {};
+    };
+
+}
+
+#endif  // MUS_INTERNAL_MUS_IUSECASE_ICHAT_MESSAGE_USE_CASE_H_
--- lib/mus-internal/mus-usecase/include/mus-usecase/chat_use_case.h
+++ lib/mus-internal/mus-usecase/include/mus-usecase/chat_use_case.h
@@ -2,7 +2,6 @@
 #define MUS_INTERNAL_MUS_USECASE_CHAT_USE_CASE_H_
 
 #include "mus-iusecase/ichat_use_case.h"
-#include "mus-irepo/ichat_message_repository.h"
 #include "mus-irepo/ichat_repository.h"
 
 namespace music_share {
@@ -11,20 +10,13 @@ class ChatUseCase : public IChatUseCase {
 public:
     ChatUseCase() = delete;
 
-    ChatUseCase(const IChatRepository &chat_rep,
-                const IChatMessageRepository &chat_message_rep);
+    ChatUseCase(const IChatRepository &chat_rep);
 
     ChatUseCase(const ChatUseCase &chat_use_case);
 
     ChatUseCase &operator=(const ChatUseCase &chat_use_case);
 
     std::optional <uint32_t> Create(ChatRequestDTO chat) override;
-
-    bool SendMessage(MessageRequestDTO message, uint32_t chat_id,
-                     uint32_t user_id) override;
-
-    std::vector <MessageResponseDTO> GetUserMessages(uint32_t user_id,
-                                                     uint32_t chat_id) override;
 
     std::vector <ChatResponseDTO> GetByIdOfOneUser(uint32_t id) override;
 
@@ -35,7 +27,6 @@ public:
 
 private:
     IChatRepository &m_chat_rep;
-    IChatMessageRepository &m_chat_message_rep;
 };
 
 } // namespace music_share
