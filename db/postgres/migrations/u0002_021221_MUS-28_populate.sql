1f4094b370f119eb320a053e1d62a976dafd0d43
Rostislav V <sunz89@yandex.ru>
2021-12-02 16:12:06 +0300

    MUS-25: treat db scripts as migrations
    (cherry picked from commit ccf4c8f05ff916484438e2335d0942f7eeb6580e)

---
+++ db/postgres/migrations/u0002_021221_MUS-28_populate.sql
@@ -0,0 +1,7 @@
+DELETE FROM mus_chat_message;
+DELETE FROM mus_chat;
+DELETE FROM mus_user_has_playlist;
+DELETE FROM mus_playlist_has_song;
+DELETE FROM mus_playlist;
+DELETE FROM mus_song;
+DELETE FROM mus_user;
