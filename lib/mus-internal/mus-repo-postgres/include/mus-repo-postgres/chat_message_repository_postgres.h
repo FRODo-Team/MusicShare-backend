3e5512b0ed57d588c9161307be3efd87697ded99
Rostislav V <sunz89@yandex.ru>
2021-12-03 20:54:27 +0300

    MUS-34: impl ChatMessage postgres repository
    (cherry picked from commit ffc25dee4f1e64161301d54e2309def0208f2a8b)

--- lib/mus-internal/mus-repo-postgres/include/mus-repo-postgres/chat_message_repository_postgres.h
+++ lib/mus-internal/mus-repo-postgres/include/mus-repo-postgres/chat_message_repository_postgres.h
@@ -2,29 +2,34 @@
 #define MUS_INTERNAL_MUS_REPO_POSTGRES_CHAT_MESSAGE_REPOSITORY_POSTGRES_H_
 
 #include "mus-irepo/ichat_message_repository.h"
-#include "db_connection_postgres.h"
+#include "internal/repository_postgres.h"
 
 namespace music_share {
 
 class ChatMessageRepositoryPostgres final : public IChatMessageRepository {
 public:
-    explicit ChatMessageRepositoryPostgres(const std::string& connection);
+    explicit ChatMessageRepositoryPostgres(const std::string& connection_string);
+    ChatMessageRepositoryPostgres(const ChatMessageRepositoryPostgres&) = delete;
     ~ChatMessageRepositoryPostgres() = default;
 
+    ChatMessageRepositoryPostgres& operator=(const ChatMessageRepositoryPostgres&) = delete;
+
     std::optional<ChatMessage> Find(uint32_t id) override;
-    void Insert(ChatMessage& out_chat_message) override;
-    void Update(const ChatMessage& chat_message) override;
-    void Delete(const ChatMessage& chat_message) override;
+    void Insert(ChatMessage& out_obj) override;
+    void Update(const ChatMessage& obj) override;
+    void Delete(const ChatMessage& obj) override;
 
     std::vector<ChatMessage> FindByChatId(uint32_t chat_id) override;
 
 private:
-    class Mapper {
+    class SqlMapper {
     public:
-        static ChatMessage ToDomain(const pqxx::row& record);
+        static ChatMessage ToDomainObject(const pqxx::row& row);
+        static SqlObject ToSqlObject(const ChatMessage& domain);
     };
 
-    DbConnectionPostgres m_database;
+    RepositoryPostgres<ChatMessage, SqlMapper> m_crud_repository;
+    const std::string& m_table_name;
 };
 
 } // namespace music_share
