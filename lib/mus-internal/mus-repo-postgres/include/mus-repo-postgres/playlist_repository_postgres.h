1fd95c43ffb2db54c9c2ba6d80f52ec9783dbf6d
Rostislav V <sunz89@yandex.ru>
2021-12-03 20:54:57 +0300

    MUS-34: impl Playlist postgres repository
    (cherry picked from commit 6ac5bae7ee9ed3e8aefcdc995966bea548d9ba61)

--- lib/mus-internal/mus-repo-postgres/include/mus-repo-postgres/playlist_repository_postgres.h
+++ lib/mus-internal/mus-repo-postgres/include/mus-repo-postgres/playlist_repository_postgres.h
@@ -2,29 +2,43 @@
 #define MUS_INTERNAL_MUS_REPO_POSTGRES_PLAYLIST_REPOSITORY_POSTGRES_H_
 
 #include "mus-irepo/iplaylist_repository.h"
-#include "db_connection_postgres.h"
+#include "internal/repository_postgres.h"
+
 
 namespace music_share {
 
 class PlaylistRepositoryPostgres final : public IPlaylistRepository {
 public:
-    explicit PlaylistRepositoryPostgres(const std::string& connection);
+    explicit PlaylistRepositoryPostgres(const std::string &connection_string);
+    PlaylistRepositoryPostgres(const PlaylistRepositoryPostgres &) = delete;
     ~PlaylistRepositoryPostgres() = default;
 
-    std::optional<Playlist> Find(uint32_t id) override;
-    void Insert(Playlist& out_playlist) override;
-    void Update(const Playlist& playlist) override;
-    void Delete(const Playlist& playlist) override;
+    PlaylistRepositoryPostgres &operator=(const PlaylistRepositoryPostgres &) = delete;
 
+    std::optional<Playlist> Find(uint32_t id) override;
+    void Insert(Playlist &out_obj) override;
+    void Update(const Playlist &obj) override;
+    void Delete(const Playlist &obj) override;
+
+    std::vector<Playlist> FindByTitle(const std::string& title) override;
     std::vector<Playlist> FindByUserId(uint32_t user_id) override;
+    std::vector<Playlist> FindByCreatorId(uint32_t creator_id) override;
 
 private:
-    class Mapper {
+    class SqlMapperForTablePlaylist {
     public:
-        static Playlist ToDomain(const pqxx::row& record);
+        // основные таблицы
+        static Playlist ToDomainObject(const pqxx::row& row);
+        static SqlObject ToSqlObject(const Playlist& domain);
     };
 
-    DbConnectionPostgres m_database;
+    RepositoryPostgres<Playlist, SqlMapperForTablePlaylist> m_crud_repository;
+    const std::string& m_table_name;
+
+    static constexpr std::string_view kUserHasPlaylistTableName =
+            "mus_user_has_playlist";
+    static constexpr std::string_view kPlaylistHasSongTableName =
+            "mus_playlist_has_song";
 };
 
 } // namespace music_share
