4e26853007551b806ffb777c4ee3a56d8020281c
Darya Vlaskina <vlaskinadarja@rambler.ru>
2021-12-05 22:15:07 +0300

    MUS-33: add realization for playlist usecase

---
+++ lib/mus-internal/mus-usecase/src/playlist_use_case.cc
@@ -0,0 +1,158 @@
+#include "mus-usecase/playlist_use_case.h"
+
+#include <memory>
+#include <optional>
+
+using std::make_unique;
+using std::nullopt;
+using std::optional;
+using std::string;
+using std::unique_ptr;
+using std::vector;
+
+namespace  music_share {
+
+    PlaylistUseCase::PlaylistUseCase(IPlaylistRepository& playlist_rep)
+                                    : m_playlist_rep(playlist_rep) { }
+
+    PlaylistUseCase::PlaylistUseCase(const PlaylistUseCase& playlist_use_case)
+                                    : m_playlist_rep(playlist_use_case.m_playlist_rep) { }
+
+    PlaylistUseCase &PlaylistUseCase::operator=(const PlaylistUseCase& playlist_use_case) {
+        m_playlist_rep = playlist_use_case.m_playlist_rep;
+        return *this;
+    }
+
+    uint32_t PlaylistUseCase::Create(uint32_t user_id,
+                                     const PlaylistRequestDTO& playlist_dto) {
+        auto playlist = make_unique<Playlist>(playlist_dto.name,
+                                                                user_id,
+                                                                playlist_dto.song_ids);
+
+        m_playlist_rep.Insert(*playlist);
+
+        if (!playlist->GetId()) {
+            throw "Can`t create playlist";
+        }
+        return *playlist->GetId();
+    }
+
+    void PlaylistUseCase::DeleteById(uint32_t user_id,
+                                     uint32_t playlist_id) {
+        optional<Playlist> playlist = m_playlist_rep.Find(playlist_id);
+
+        if (!playlist) {
+            throw "Playlist doesn`t exist";
+        }
+        if (playlist->GetCreatorId() != user_id) {
+            throw "No access";
+        }
+
+        m_playlist_rep.Delete(*playlist);
+    }
+
+    void PlaylistUseCase::AddSongById(const PlaylistSongRequestDTO& song,
+                                      uint32_t playlist_id,
+                                      uint32_t user_id) {
+        optional<Playlist> playlist = m_playlist_rep.Find(playlist_id);
+
+        if (!playlist) {
+            throw "Playlist doesn`t exist";
+        }
+        if (playlist->GetCreatorId() != user_id) {
+            throw "No access";
+        }
+
+        for (const uint32_t& id : song.song_ids) {
+            playlist->AppendSong(id);
+        }
+        m_playlist_rep.Update(*playlist);
+    }
+
+    void PlaylistUseCase::DeleteSongById(uint32_t song_id,
+                                         uint32_t playlist_id,
+                                         uint32_t user_id) {
+        optional<Playlist> playlist = m_playlist_rep.Find(playlist_id);
+
+        if (!playlist) {
+            throw "Playlist doesn`t exist";
+        }
+        if (playlist->GetCreatorId() != user_id) {
+            throw "No access";
+        }
+
+        playlist->RemoveSong(song_id);
+        m_playlist_rep.Update(*playlist);
+    }
+
+    vector<PlaylistResponseDTO> PlaylistUseCase::GetByUserId(uint32_t user_id) {
+        vector<Playlist> playlists = m_playlist_rep.FindByUserId(user_id);
+
+        if (playlists.empty()) {
+            throw "Playlist doesn`t exist";
+        }
+
+        vector<PlaylistResponseDTO> playlists_dto;
+        playlists_dto.reserve(playlists.size());
+        for (const Playlist& playlist : playlists) {
+            if (!playlist.GetId()) {
+                throw "Null id";
+            }
+            playlists_dto.emplace_back(*playlist.GetId(),
+                                    playlist.GetCreatorId(),
+                                    static_cast<uint32_t>(playlist.GetSongIds().size()),
+                                    playlist.GetTitle());
+        }
+
+        return playlists_dto;
+    }
+
+    vector<PlaylistResponseDTO> PlaylistUseCase::GetByTitle(const string& title) {
+        vector<Playlist> playlists = m_playlist_rep.FindByTitle(title);
+
+        if (playlists.empty()) {
+            throw "Playlist doesn`t exist";
+        }
+
+        vector<PlaylistResponseDTO> playlists_dto;
+        playlists_dto.reserve(playlists.size());
+        for (const Playlist& playlist : playlists) {
+            if (!playlist.GetId()) {
+                throw "Null id";
+            }
+            playlists_dto.emplace_back(*playlist.GetId(),
+                                       playlist.GetCreatorId(),
+                                       static_cast<uint32_t>(playlist.GetSongIds().size()),
+                                       playlist.GetTitle());
+        }
+
+        return playlists_dto;
+    }
+
+    PlaylistResponseDTO PlaylistUseCase::GetById(uint32_t id) {
+        optional<Playlist> playlist = m_playlist_rep.Find(id);
+
+        if (!playlist) {
+            throw "Playlist doesn`t exist";
+        }
+        if (!playlist->GetId()) {
+            throw "Null id";
+        }
+
+        return PlaylistResponseDTO(*playlist->GetId(),
+                                   playlist->GetCreatorId(),
+                                   static_cast<uint32_t>(playlist->GetSongIds().size()),
+                                   playlist->GetTitle());
+    }
+
+    vector<uint32_t> PlaylistUseCase::GetSongs(uint32_t playlist_id) {
+        optional<Playlist> playlist = m_playlist_rep.Find(playlist_id);
+
+        if (!playlist) {
+            throw "Playlist doesn`t exist";
+        }
+
+        return playlist->GetSongIds();
+    }
+
+}  // namespace music_share
