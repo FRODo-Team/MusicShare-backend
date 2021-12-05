2ba11b57701886207c636889dcd763c71538850d
rflban <nabiev.alsn@yandex.ru>
2021-12-05 22:45:16 +0300

    MUS-35: add router

--- lib/http/include/http/common/message.h
+++ lib/http/include/http/common/message.h
@@ -1,4 +1,17 @@
 #ifndef MUS_HTTP_COMMON_MESSAGE_H_
 #define MUS_HTTP_COMMON_MESSAGE_H_
 
+namespace music_share {
+namespace http {
+namespace common {
+
+class Message {
+public:
+protected:
+};
+
+}  // namespace common
+}  // namespace http
+}  // namespace music_share
+
 #endif  // MUS_HTTP_COMMON_MESSAGE_H_
--- lib/http/include/http/common/response.h
+++ lib/http/include/http/common/response.h
@@ -1,11 +1,13 @@
 #ifndef MUS_HTTP_COMMON_RESPONSE_H_
 #define MUS_HTTP_COMMON_RESPONSE_H_
 
+#include "http/common/message.h"  // http::common::Message
+
 namespace music_share {
 namespace http {
 namespace common {
 
-class Response {
+class Response : public Message {
 };
 
 }  // namespace common
