b60781dc724b27aa280173d692408bd2f1ba645f
Rostislav V <sunz89@yandex.ru>
2021-12-09 17:17:00 +0300

    MUS-24: add ChatMessage param constructor realization, change interface for ichat repo
    (cherry picked from commit aa97016bfb9cb62e0896360b951c5749917c5aca)

--- lib/mus-internal/mus-irepo/include/mus-irepo/ichat_repository.h
+++ lib/mus-internal/mus-irepo/include/mus-irepo/ichat_repository.h
@@ -14,8 +14,8 @@ public:
     virtual std::vector<Chat> FindByUserId(uint32_t user_id) = 0;
 
     /* Возвращает чат-диалог двух пользователей. */
-    virtual Chat FindByIdsOfUserPair(uint32_t first_user_id,
-                                     uint32_t second_user_id) = 0;
+    virtual std::optional<Chat> FindByIdsOfUserPair(uint32_t first_user_id,
+                                                    uint32_t second_user_id) = 0;
 };
 
 } // namespace music_share
