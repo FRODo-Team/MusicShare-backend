a65ab13073d84f86b31c9e2fc0a91fcd15f39bba
Darya Vlaskina <vlaskinadarja@rambler.ru>
2021-12-19 18:47:09 +0300

    MUS-33: add field playlist id in message dto

--- lib/mus-internal/mus-dto/include/mus-dto/message_response_dto.h
+++ lib/mus-internal/mus-dto/include/mus-dto/message_response_dto.h
@@ -12,9 +12,12 @@ struct MessageResponseDTO {
     MessageResponseDTO(uint32_t id_, uint32_t chat_id_,
                        uint32_t sender_id_,
                        const std::string& content_,
-                       const std::string& datetime_)
+                       const std::string& datetime_,
+                       std::optional<uint32_t> playlist_id_ = {})
                        : id(id_), chat_id(chat_id_), sender_id(sender_id_),
-                       content(content_), datetime(datetime_) {}
+                       content(content_), datetime(datetime_),
+                       playlist_id(playlist_id_)
+                       {}
 
     uint32_t id;
     uint32_t chat_id;
